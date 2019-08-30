77397e61188612f4027b7b60b950200df75ad197
Laurent Grenier <laurent.grenier.pro@gmail.com>
2019-08-30 17:22:54 +0200

    assesment

--- multivariate_calculus/draft.py
+++ multivariate_calculus/draft.py
@@ -207,6 +207,116 @@ def my_animation():
                          interval=100, blit=True)
     plt.show()
 
+# PACKAGE: DO NOT EDIT THIS CELL
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+matplotlib.style.use('fivethirtyeight')
+from sklearn.datasets import fetch_lfw_people, fetch_olivetti_faces
+import time
+import timeit
+
+def mean_naive(X):
+    "Compute the mean for a dataset X nby iterating over the data points"
+    # X is of size (D,N) where D is the dimensionality and N the number of data points
+    D, N = X.shape
+
+    mean = np.zeros((D,1))
+    ### Edit the code; iterate over the dataset and compute the mean vector.
+    for n in range(N):
+        # Update the mean vector
+        mean = mean + (X[:,n] / N).reshape(D,1)
+    ###
+
+    return mean
+
+def test_mean_naive():
+    image_shape = (64, 64)
+    # Load faces data
+    dataset = fetch_olivetti_faces('./')
+    faces = dataset.data.T
+
+    print('Shape of the faces dataset: {}'.format(faces.shape))
+    print('{} data points'.format(faces.shape[1]))
+
+    mean = mean_naive(faces)
+    print("mean: {}".format(mean.shape))
 
 
+def cov_naive(X):
+    """Compute the covariance for a dataset of size (D,N)
+    where D is the dimension and N is the number of data points"""
+    D, N = X.shape
+
+    ### Edit the code below to compute the covariance matrix by iterating over the dataset.
+    covariance = np.zeros((D, D))
+
+    ### Update covariance
+    mean = mean_naive(X)
+
+    # for each sample
+    for n in range(N):
+        # calculate for each axes
+        for i in range(D):
+            # the new value with each other axes
+            for j in range(D):
+                # covariance 0 is the one of x with each other dimensions j
+                # divided by N-1 like it is in Numpy Cov instead of N
+                covariance[i,j] = covariance[i,j] + (X[i, n] - mean[i][0]) * (X[j, n] - mean[j][0]) / (N-1)
+    ###
+    return covariance
+
+
+A = np.array([[1,4,2],[3,8,3],[1,2,1],[1,2,1]]).T
+B = np.array([[1,2], [4,2], [1,2]])
+
+# print("cov with np.cov: ", np.cov(A))
+# print("cov with cov_naive: ", cov_naive(A))
+
+
+def mean(X):
+    "Compute the mean for a dataset of size (D,N) where D is the dimension and N is the number of data points"
+    # given a dataset of size (D, N), the mean should be an array of size (D,1)
+    # you can use np.mean, but pay close attention to the shape of the mean vector you are returning.
+    D, N = X.shape
+    ### Edit the code to compute a (D,1) array `mean` for the mean of dataset.
+    mean = np.zeros((D, 1))
+    ### Update mean here
+    mean = np.mean(X, axis=1).reshape(D, 1)
+
+    ###
+    return mean
+
+
+def cov(X):
+    "Compute the covariance for a dataset"
+    # X is of size (D,N)
+    # It is possible to vectorize our code for computing the covariance with matrix multiplications,
+    # i.e., we do not need to explicitly
+    # iterate over the entire dataset as looping in Python tends to be slow
+    # We challenge you to give a vectorized implementation without using np.cov, but if you choose to use np.cov,
+    # be sure to pass in bias=True.
+    D, N = X.shape
+    ### Edit the code to compute the covariance matrix
+
+    ### Update covariance_matrix here
+    mu = mean(X)
+    M = X - mean(X)
+    covariance_matrix =  (M @ M.T) / (N-1)
+
+    ###
+    return covariance_matrix
+
+A = np.array([[1,4,2],[3,8,3],[2,5,2]]).T
+print("np.cov: ", np.cov(A))
+print("cov: ", cov(A))
+
+image_shape = (64, 64)
+# Load faces data
+dataset = fetch_olivetti_faces('./')
+faces = dataset.data.T
+
+#np.testing.assert_almost_equal(mean(faces), mean_naive(faces), decimal=6)
+np.testing.assert_almost_equal(cov(faces), cov_naive(faces))
 
